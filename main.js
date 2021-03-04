3f0cb77c323a7dbda0933c34fed7997f88967183
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-03-04 23:23:09 +0530

    total http requests increment counter

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const prom = require('prom-client')
 
+const APP_NAME = 'node_server'
 const PORT = 5000
 
 const app = express()
 const registry = new prom.Registry()
 prom.collectDefaultMetrics({ register: registry })
 
+const counter = new prom.Counter({
+    name: `${APP_NAME}_http_requests_total`,
+    help: `Number of HTTP requests received by the server so far`,
+    labelNames: ['method', 'path']
+})
+registry.registerMetric(counter)
+
 app.use((req, res, next) => {
     console.log(new Date().toLocaleString(), req.originalUrl)
+    counter.inc({ method: req.method, path: req.originalUrl })
     next()
 })
 app.get('/one', (req, res) => {
