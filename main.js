5dccc0ebf7c2734c57d8df8188101a7ffc3f31c7
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-03-04 22:28:04 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const prom = require('prom-client')
+
+const app = express()
+const Registry = prom.Registry
+const registry = new Registry()
+prom.collectDefaultMetrics({ register: registry })
+
+app.use((req, res, next) => {
+    console.log(new Date().toLocaleString(), req.originalUrl)
+    next()
+})
+app.get('/one', (req, res) => {
+    res.send({
+        path: '/one'
+    })
+})
+app.get('/two', (req, res) => {
+    res.send({
+        path: '/two'
+    })
+})
+app.get('/metrics', async (req, res) => {
+    const metrics = await registry.metrics()
+    res.type('text/plain')
+    res.send(metrics)
+})
+
+app.listen(5000, () => {
+    console.log('Listening...')
+})
